1a73e6ea6ed35b35fe0f414c48566aefc0c8ae7d
Ravi Sharma <ruravi80@gmail.com>
2023-01-31 23:00:39 +0530

    check connection for placing orders between client and server

--- controllers/order.controllers.js
+++ controllers/order.controllers.js
@@ -4,33 +4,37 @@ const Product = require("../models/product.model");
 const sendEmail = require("../utils/emailer");
 
 module.exports.generateOrder = async (req, res) => {
-  try {
-    const { id } = req.body;
-    const product = await Product.findById(id);
-    const user = req.user;
-    const newOrder = await Order.create({ user, product });
-    user.orders.push(newOrder);
-    await user.save();
-    sendEmail(
-      user.username,
-      "Order placed successfully",
-      `
-    Your order for ${product.brand} ${product.model} ${product.ram}gb + ${product.storage}gb
-    color ${product.color} is placed successfully.
+  const {orders} = req.body;
+  console.log(orders);
+  res.send(orders);
 
-    Your order id is "${newOrder._id}"
-    Our executive will contact you shortly.
-    Thank you for using our service.
-    www.mrphonex.com
-    `
-    );
-    product.units -= 1;
-    await product.save();
-    res.send({ user });
-  } catch (error) {
-    console.log(error);
-    res.send({ error });
-  }
+  // try {
+  //   const { id } = req.body;
+  //   const product = await Product.findById(id);
+  //   const user = req.user;
+  //   const newOrder = await Order.create({ user, product });
+  //   user.orders.push(newOrder);
+  //   await user.save();
+  //   sendEmail(
+  //     user.username,
+  //     "Order placed successfully",
+  //     `
+  //   Your order for ${product.brand} ${product.model} ${product.ram}gb + ${product.storage}gb
+  //   color ${product.color} is placed successfully.
+
+  //   Your order id is "${newOrder._id}"
+  //   Our executive will contact you shortly.
+  //   Thank you for using our service.
+  //   www.mrphonex.com
+  //   `
+  //   );
+  //   product.units -= 1;
+  //   await product.save();
+  //   res.send({ user });
+  // } catch (error) {
+  //   console.log(error);
+  //   res.send({ error });
+  // }
 };
 
 module.exports.getOrdersByIds = async (req, res) => {
